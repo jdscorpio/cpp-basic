d36f1140b0930c9dfa37efa7ae22844364957537
Jaroslaw Drzeżdżon <jdrzezdzon@gmail.com>
2024-03-11 09:04:22 +0100

    Add files via upload

--- pole_trojkata.cpp
+++ pole_trojkata.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -11,28 +12,23 @@ struct Color
 
 struct Point
 {
-    double x;
+    double x; // współrzędna x punkt
     double y;
     Color color;
 };
 
 double area_triangle(Point A, Point B, Point C)
 {
-    return 0.5 * abs((B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x));
+    return 0.5 * fabs((B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x));
 }
-
 int main()
 {
     Point A, B, C;
-    cout << "Podaj punkt A: ";
     cin >> A.x >> A.y;
-    cout << "Podaj punkt B: ";
     cin >> B.x >> B.y;
-    cout << "Podaj punkt C: ";
     cin >> C.x >> C.y;
-
     cout << area_triangle(A, B, C) << "\n";
-    A.color.b = 45;
+    A.color.b = 234;
 
     return 0;
 }
--- pole_wielokata.cpp
+++ pole_wielokata.cpp
@@ -1,19 +1,5 @@
-/*
-Pole wielokąta
-
-Dane wejściowe
-6
--5 1
--3 4
-1 5
-3 2
-2 -2
--2 -3
-Dane wyjściowe
-4
-*/
-
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -25,25 +11,29 @@ struct Point
 
 double area_triangle(Point A, Point B, Point C)
 {
-    return 0.5 * abs((B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x));
+    return 0.5 * fabs((B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x));
 }
+
 double area_polygon(int n, Point points[])
 {
     double area = 0;
-    for (int i = 1; i < n - 1; i++)
+    for (int i = 0; i < n - 1; i++)
+    {
         area += area_triangle(points[0], points[i], points[i + 1]);
+    }
     return area;
 }
-
 int main()
 {
     int n;
-    cout << "Podaj liczbe punktow: ";
+    cout << "Podaj liczbe wierzcholkow: ";
     cin >> n;
+    cout << "Podaj wierzcholki: ";
     Point points[n];
     for (int i = 0; i < n; i++)
         cin >> points[i].x >> points[i].y;
 
     cout << area_polygon(n, points);
+
     return 0;
 }
